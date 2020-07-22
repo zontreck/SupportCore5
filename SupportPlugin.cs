b54686314e6cceddc915195c7bfdb9d516c93ecf
Zontreck <tara@zontreck.dev>
2020-07-21 19:32:28 -0700

    Create base SupportPlugin

---
+++ SupportPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bot;
+using OpenMetaverse;
+
+
+namespace SupportCore5
+{
+    class SupportPlugin : IProgram
+    {
+        public string ProgramName
+        {
+            get
+            {
+                return "SupportCore5";
+            }
+        }
+
+        public float ProgramVersion => 1.0f;
+
+        public void getTick()
+        {
+        }
+
+        public void LoadConfiguration()
+        {
+        }
+
+        public void onIMEvent(object sender, InstantMessageEventArgs e)
+        {
+        }
+
+        public void passArguments(string data)
+        {
+        }
+
+        public void run()
+        {
+        }
+    }
+}
