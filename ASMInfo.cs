0d8ea08f66423a5f7b3054a160eb61adce049e47
Zontreck <tara@zontreck.dev>
2020-08-31 03:23:48 -0700

    Fix a typo

--- ASMInfo.cs
+++ ASMInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("ZNI")]
 [assembly: AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
 [assembly: AssemblyCopyright("(C) 2020 Tara Piccari")]
-[assembly: AssemblyFileVersion("1.0.0.0499")]
+[assembly: AssemblyFileVersion("1.0.0.0500")]
 [assembly: AssemblyDescription("Support Core5 Plugin for BotCore")]
--- Support.cs
+++ Support.cs
@@ -16,7 +16,7 @@ namespace SupportCore5
                 return;
             } else
             {
-                if (SupportCore.Singleton.SupportActive)
+                if (!SupportCore.Singleton.SupportActive)
                 {
                     // Read the auto response from the auto reply text doc
 
@@ -31,24 +31,17 @@ namespace SupportCore5
                                 Destinations source,
                                 UUID agentKey, string agentName)
         {
-            try
+            string TheString = "";
+            foreach (string s in additionalArgs)
             {
-
-                string TheString = "";
-                foreach (string s in additionalArgs)
-                {
-                    TheString += s + " ";
-                }
-
-                if (TheString.EndsWith(' ')) TheString.TrimEnd(' ');
-
-                SupportSettings.Instance.AutoReply = TheString;
-                MHE(source, client, "Auto reply set");
-                SupportSettings.SaveMemory();
-            }catch(Exception e)
-            {
-                MHE(source, client, e.Message+"\n\n"+e.StackTrace);
+                TheString += s + " ";
             }
+
+            if (TheString.EndsWith(' ')) TheString.TrimEnd(' ');
+
+            SupportSettings.Instance.AutoReply = TheString;
+            MHE(source, client, "Auto reply set");
+            SupportSettings.SaveMemory();
         }
     }
 }
