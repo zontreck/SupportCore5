8be5e899a55eb8c034d0fa874adb5260938c0300
Zontreck <tara@zontreck.dev>
2020-09-04 02:43:49 -0700

    Add new asm info

--- ASMInfo.cs
+++ ASMInfo.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
+using LibZNI;
 
 [assembly: AssemblyCompany("ZNI")]
 [assembly: AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
 [assembly: AssemblyCopyright("(C) 2020 Tara Piccari")]
-[assembly: AssemblyFileVersion("1.0.0.0500")]
+[assembly: AssemblyFileVersion("1.0.0.0501")]
 [assembly: AssemblyDescription("Support Core5 Plugin for BotCore")]
+[assembly: AutoUpdater("/job/ZBotCore/job/SupportCore5", "module.tar")]
